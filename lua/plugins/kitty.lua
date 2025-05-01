18570057bc3a8900a737dc0ab96dcb2ede32099d
SilverofLight <a2061686759@outlook.com>
2025-05-01 11:40:07 +0800

    kitty fix

--- lua/plugins/kitty.lua
+++ lua/plugins/kitty.lua
@@ -6,7 +6,24 @@ return {
   event = { 'User KittyScrollbackLaunch' },
   -- version = '*', -- latest stable version, may have breaking changes if major version changed
   -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
+  -- vim.keymap.set({ 'v' }, 'Y', '<Plug>(KsbVisualYankLine)', {}),
+  -- vim.keymap.set({ 'v' }, 'y', '<Plug>(KsbVisualYank)', {}),
+  -- vim.keymap.set({ 'n' }, 'Y', '<Plug>(KsbNormalYankEnd)', {}),
+  -- vim.keymap.set({ 'n' }, 'y', '<Plug>(KsbNormalYank)', {}),
+  -- vim.keymap.set({ 'n' }, 'yy', '<Plug>(KsbYankLine)', {}),
+  --
+  -- vim.keymap.set({ 'n' }, 'q', '<Plug>(KsbCloseOrQuitAll)', {}),
+  -- vim.keymap.set({ 'n', 't', 'i' }, 'ZZ', '<Plug>(KsbQuitAll)', {}),
+  --
+  -- vim.keymap.set({ 'n' }, '<tab>', '<Plug>(KsbToggleFooter)', {}),
+  -- vim.keymap.set({ 'n', 'i' }, '<cr>', '<Plug>(KsbExecuteCmd)', {}),
+  -- vim.keymap.set({ 'n', 'i' }, '<c-v>', '<Plug>(KsbPasteCmd)', {})
   config = function()
+    vim.keymap.set({ 'v' }, 'Y', '<Plug>(KsbVisualYankLine)', {})
+    vim.keymap.set({ 'v' }, 'y', '<Plug>(KsbVisualYank)', {})
+    vim.keymap.set({ 'n' }, 'Y', '<Plug>(KsbNormalYankEnd)', {})
+    vim.keymap.set({ 'n' }, 'y', '<Plug>(KsbNormalYank)', {})
+    vim.keymap.set({ 'n' }, 'yy', '<Plug>(KsbYankLine)', {})
     require('kitty-scrollback').setup()
-  end,
+  end
 }
