d5c7c175ee589ceba817f14625d8dcb71bede41c
SilverofLight <a2061686759@outlook.com>
2025-05-15 15:03:49 +0800

    lsp & vimtex feat

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,8 +6,21 @@ return {
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "zathura"
     vim.g.vimtex_compiler_method = 'xelatex'
+    vim.g.vimtex_mappings_enabled = false
     vim.g.vimtex_compiler_latexmk_engines = {
       _ = '-xelatex'
     }
+    vim.g.vimtex_toc_config = {
+      layer_status = {
+        content = 1,
+        label = 0,
+        todo = 1,
+        include = 1
+      },
+      show_help = 0,
+      split_width = 30,
+    }
+    vim.keymap.set('n', '<leader>ll', '<plug>(vimtex-compile)')
+    vim.keymap.set('n', '<leader>j', '<plug>(vimtex-toc-toggle)')
   end
 }
