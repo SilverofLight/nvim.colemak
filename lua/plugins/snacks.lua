a9819183fc383d218a7af3d7a43f1d96937b1c55
SilverofLight <a2061686759@outlook.com>
2025-05-23 19:42:22 +0800

    snacks: quickfile, dashboard, notifier

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,5 +13,17 @@ return {
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     },
+    quickfile = {
+
+    },
+    dashboard = {
+      -- your dashboard configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    notifier = {
+      top_down = false,
+      margin = { top = 0, right = 1, bottom = 2 }
+    },
   }
 }
