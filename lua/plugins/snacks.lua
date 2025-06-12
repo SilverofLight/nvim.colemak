4c49ad222a1baed14a734b0211abb449f4b251ed
SilverofLight <a2061686759@outlook.com>
2025-06-12 09:27:18 +0800

    snacks-statuscolumn

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -58,6 +58,19 @@ return {
     },
     words = {
       vim.keymap.set("n", "gw", "<cmd>lua Snacks.words.jump()<cr>", { desc = "Next Reference" }),
+    },
+    statuscolumn = {
+      left = { "mark", "sign" }, -- priority of signs on the left (high to low)
+      right = { "fold", "git" }, -- priority of signs on the right (high to low)
+      folds = {
+        open = false,        -- show open fold icons
+        git_hl = false,      -- use Git Signs hl for fold icons
+      },
+      git = {
+        -- patterns to match Git signs
+        patterns = { "GitSign", "MiniDiffSign" },
+      },
+      refresh = 50, -- refresh at most every 50ms
     }
   }
 }
