ca750a294f7d204152d769ec829dd4ed4b01e56b
SilverofLight <a2061686759@outlook.com>
2025-07-17 09:37:57 +0800

    mpv update

--- lua/plugins/mpv.lua
+++ lua/plugins/mpv.lua
@@ -12,7 +12,7 @@ return {
   -- set your keybindings below
   vim.keymap.set("n", "<leader>vc", "<cmd>MpvCopyStamp<CR>", { desc = "Copy Mpv Note" }),
   vim.keymap.set("n", "<leader>vp", "<cmd>MpvPasteStamp<CR>", { desc = "Paste Mpv Note" }),
-  vim.keymap.set("n", "<leader>vi", "<cmd>MpvPasteStamp<CR>", { desc = "Paste Mpv Note" }),
+  vim.keymap.set("n", "<leader>vi", "<cmd>MpvPasteImage<CR>", { desc = "Paste Detected Image" }),
   vim.keymap.set("n", "<leader>vo", "<cmd>MpvOpenStamp<CR>", { desc = "Open Mpv Note" }),
   vim.keymap.set("n", "<leader>vh", "<cmd>MpvHover<CR>", { desc = "Mpv Hover" }),
   vim.keymap.set({ "n", "i" }, "<C-j>", "<cmd>MpvTogglePause<CR>", { desc = "Mpv Toggle Pause" })
--- lua/plugins/tableMode.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'Kicamon/markdown-table-mode.nvim',
-  event = "VeryLazy",
-  config = function()
-    require('markdown-table-mode').setup()
-  end
-}
