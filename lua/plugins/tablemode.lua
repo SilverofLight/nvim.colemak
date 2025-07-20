2f887119565042bb59f7c4b7d0e0c6fe705d5139
SilverofLight <a2061686759@outlook.com>
2025-07-20 10:33:16 +0800

    tablemode: lazy

--- lua/plugins/tablemode.lua
+++ lua/plugins/tablemode.lua
@@ -2,6 +2,8 @@ vim.g.table_mode_disable_mappings = 1
 vim.g.table_mode_disable_tableize_mappings = 1
 return {
   "dhruvasagar/vim-table-mode",
+  event = "VeryLazy",
+  ft = "markdown",
 
   -- vim.keymap.set("n", "tm", "<cmd>TableModeToggle<CR>", { desc = "Toggle Table Mode" }),
   config = function()
