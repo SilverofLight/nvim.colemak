c52025e198305c5d168e50176a7a673f12a982fc
SilverofLight <a2061686759@outlook.com>
2026-05-30 09:49:15 +0800

    gdscript lsp

--- lua/config/pluginConfig.lua
+++ lua/config/pluginConfig.lua
@@ -237,3 +237,10 @@ vim.lsp.config("pyright", {
 		},
 	},
 })
+
+vim.lsp.config("gdscript", {
+    cmd = vim.lsp.rpc.connect( "127.0.0.1", 6005 ),
+    filetypes = { "gd", "gdscript" },
+    root_markers = { "project.godot" },
+})
+vim.lsp.enable( "gdscript" )
